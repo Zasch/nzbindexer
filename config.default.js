02aa5b014e3dadf55618fa05128760750468f339
Sander Spilleman <sander@spilleman.nl>
2017-05-02 08:36:37 +0200

    initial version

--- config.default.js
+++ config.default.js
@@ -6,6 +6,13 @@ module.exports = {
 		port: 563,
 		secure: true
 	},
+	mongodb: 'mongodb://localhost:27017/nzbindex',
+	redis: {
+		host: 'localhost',
+		port: 6379,
+		db: 9,
+		return_buffers: true
+	},
 	group: '<alt.binaries.teevee>',
 	articles_per_connection: 25000,
 	total_articles: 1000000,
