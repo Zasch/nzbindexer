b7f06584ccd51cc4afcd717360cfc14b8c9594dc
Sander Spilleman <sander@spilleman.nl>
2017-05-12 01:39:13 +0200

    minor fixes

--- build/lib/logger.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bunyan = require("bunyan");
-const config_1 = require("../config");
-exports.logger = bunyan.createLogger(config_1.config.logger);
-//# sourceMappingURL=logger.js.map
